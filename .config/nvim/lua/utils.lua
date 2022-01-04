a6f074ae8bb878b12c83b8f23e71754ecd90ea22
Manu Farcis <emmanuel@farcis.fr>
2022-01-04 23:15:16 +0100

    add new lua files

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,6 +1,26 @@
 local M = {} -- The module to export
 local cmd = vim.cmd
 
+local opts = { noremap = true, silent = false }
+local generic_opts = {
+  insert_mode = opts,
+  normal_mode = opts,
+  visual_mode = opts,
+  visual_block_mode = opts,
+  command_mode = opts,
+  term_mode = { silent = false },
+}
+
+local mode_adapters = {
+  insert_mode = "i",
+  normal_mode = "n",
+  term_mode = "t",
+  visual_mode = "v",
+  visual_block_mode = "x",
+  command_mode = "c",
+}
+
+
 -- We will create a few autogroup, this function will help to avoid
 -- always writing cmd('augroup' .. group) etc..
 function M.create_augroup(autocmds, name)
@@ -18,6 +38,21 @@ function M.add_rtp(path)
   rtp = rtp .. ',' .. path
 end
 
+function M.set_keymaps(mode, key, val)
+  local opt = generic_opts[mode] and generic_opts[mode] or opts
+  if type(val) == "table" then
+    opt = val[2]
+    val = val[1]
+  end
+  vim.api.nvim_set_keymap(mode, key, val, opt)
+end
+
+function M.map(mode, keymaps)
+  mode = mode_adapters[mode] and mode_adapters[mode] or mode
+  for k, v in pairs(keymaps) do
+    M.set_keymaps(mode, k, v)
+  end
+end
 -- Map a key with optional options
 function M.keymap(mode, keys, action, options)
   if options == nil then
