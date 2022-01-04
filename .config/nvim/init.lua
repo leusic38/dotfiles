2e96840a9e8070e10e4bce33bc1e2778bcf13922
Manu Farcis <emmanuel@farcis.fr>
2022-01-04 23:12:44 +0100

    add new nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,10 @@
+-- Init nivim config with lua
+-- https://vonheikemen.github.io/devlog/tools/configuring-neovim-using-lua/ for help
+require("manu.plugins").setup()
+-- require "manu.options"
+-- require "manu.keymaps"
+-- require "manu.plugins"
+-- require "manu.plugins.nvim-tree"
+-- require "manu.plugins.alpha"
+-- require "manu.plugins.everforest"
+-- require "manu.plugins.refactoring.lua"
